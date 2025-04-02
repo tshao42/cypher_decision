9dbfd27ccae8fcaae93dce36d3cf35ff71d77a27
Tianyi S <tshao0602@gmail.com>
2025-04-02 00:02:56 -0700

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './', // Use relative paths,
-  assetsInclude: ['**/*.svg'],
+  assetsInclude: ['**/*.svg', '/src/assets/mermaids/*.svg'], // Include SVGs in general and specifically in /src/assets/mermaids
   build: {
     outDir: 'dist'
   }
