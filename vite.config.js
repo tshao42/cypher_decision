566b96766b671fdd2dac8cb06cf1933a02f5fd89
Tianyi S <tshao0602@gmail.com>
2025-04-01 21:36:48 -0700

    changed back the config

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default {
-  // Make sure base path is correct
-  base: '/',
-  build: {
-    outDir: 'dist' // Check if this matches your Netlify publish directory
-  }
-}
+export default defineConfig({
+  plugins: [react()],
+  base: '/cypher_decision/'
+})
