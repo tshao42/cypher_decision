3ee70e0fccba777f2f8da0283266ba4c56abcc7f
Tianyi S <tshao0602@gmail.com>
2025-04-02 00:01:04 -0700

    svg vite config change

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use relative paths
+  base: './', // Use relative paths,
+  assetsInclude: ['**/*.svg'],
   build: {
     outDir: 'dist'
   }
