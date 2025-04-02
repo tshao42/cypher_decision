616347d449d133b4314aa08349b33b6a699da27c
Tianyi S <tshao0602@gmail.com>
2025-04-02 00:06:18 -0700

    svg still not included

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,19 @@ export default defineConfig({
   base: './', // Use relative paths,
   assetsInclude: ['**/*.svg', '/src/assets/mermaids/*.svg'], // Include SVGs in general and specifically in /src/assets/mermaids
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html'
+      },
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name.endsWith('.svg')) {
+            return 'assets/[name]-[hash][extname]';
+          }
+          return 'assets/[name][extname]';
+        }
+      }
+    }
   }
 })
